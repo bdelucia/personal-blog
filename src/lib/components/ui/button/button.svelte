7c1b66cdcb6cd543709f9915ad8272ba30d3c9b9
Robert (Bobby) DeLucia <bdelucia@asu.edu>
2026-01-12 23:11:47 -0700

    chore: initial project configuration

---
+++ src/lib/components/ui/button/button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Button as ButtonPrimitive } from "bits-ui";
+	import { buttonVariants, type Props, type Events } from "./index.js";
+	import { cn } from "$lib/utils";
+
+	type $$Props = Props;
+	type $$Events = Events;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+	export let variant: $$Props["variant"] = "default";
+	export let size: $$Props["size"] = "default";
+	export let builders: $$Props["builders"] = [];
+</script>
+
+<ButtonPrimitive.Root
+	{builders}
+	class={cn(buttonVariants({ variant, size, className }))}
+	type="button"
+	{...$$restProps}
+	on:click
+	on:keydown
+>
+	<slot />
+</ButtonPrimitive.Root>
